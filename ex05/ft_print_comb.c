16f3566ee0a3d9b942f90574244a63dd37ed94c6
Eduartz <eduneoxz@hotmail.com>
2023-06-07 15:06:13 +0200

    ok print

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -1,14 +1,35 @@
 #include <unistd.h>
 
+void ft_print_tre(char a, char b, char c)
+{
+	char space = ' ';
+
+	write(1, &a, 1);
+	write(1, &b, 1);
+	write(1, &c, 1);
+	write(1, &space, 1);
+}
+
 void ft_print_comb(void)
 {
-	char numb = '1';
+    char a = '0';
 
-	while (numb <= '10')
-	{
-		write(1, &numb, 1);
-		numb++;
-	}
+    while (a <= '9')
+    {
+		char b = a + 1;
+		while (b <= '9' && b > a)
+		{
+			char c = b + 1;
+			while(c <= '9' && c > b)
+			{
+				ft_print_tre(a, b, c);
+				c++;
+			}
+			b++;
+		}
+        a++;
+    }
+    
 }
 
 int main()
